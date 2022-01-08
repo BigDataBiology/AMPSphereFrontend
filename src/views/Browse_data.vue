ddc8a97546aaa7c1bd9b9272e8cba2c18ef48342
Hui-Chong <huichong.me@gmail.com>
2022-01-08 20:06:56 +0800

    ENH update structured search

--- src/views/Browse_data.vue
+++ src/views/Browse_data.vue
@@ -5,8 +5,14 @@
       <div class="col-12 col-xl-8 justify-center q-pa-auto">
         <div class="row">
           <div class="col-12 col-md-3 q-pa-sm">
-            <div style="position: fixed; display: block; top: 180px; height: 500px">
+            <div>
               <div class="row q-px-xs q-py-xs filter-subsection-title">Filter by metadata</div>
+<!--              <div class="row q-px-md q-py-xs">-->
+<!--                <q-btn-toggle-->
+<!--                    v-model="model" spread no-caps toggle-color="primary" color="white" style="width: 250px"-->
+<!--                    text-color="black" :options="[{label: 'Option 1', value: 'one', attr: { 'aria-label': 'Button label' }}, {label: 'Option 2', value: 'two'}]"-->
+<!--                />-->
+<!--              </div>-->
               <div class="row q-px-md q-py-xs">
                 <q-select filled v-model="options.habitat" label="Habitat" @update:model-value="onHabitatChange"
                           :options="availableOptions.habitat" @filter="filterHabitat"
@@ -27,16 +33,18 @@
                 <!--                    <q-input v-model.number="options.pep_length.min" type="number" label="Min" filled style="max-width: 100px" /> &nbsp; &nbsp;-->
                 <!--                    <q-input v-model.number="options.pep_length.max" type="number" label="Max" filled style="max-width: 100px" />-->
                 <q-range v-model="options.pep_length" @change="onPepLengthChange"
-                         :min="availableOptions.pep_length.min" style="width: 240px"
-                         :max="availableOptions.pep_length.max" label color="secondary"/>
+                         :min="staticOptions.pep_length.min" :max="staticOptions.pep_length.max"
+                         :inner-min="filteredAvailableOptions.pep_length.min" style="width: 240px"
+                         :inner-max="filteredAvailableOptions.pep_length.max" label color="secondary"/>
               </div>
               <div class="row q-px-xs q-py-xs filter-subsection-title">Molecular weight:</div>
               <div class="row q-px-md">
                 <!--                    <q-input v-model.number="options.molecular_weight.min" type="number" label="Min" filled style="max-width: 100px" /> &nbsp; &nbsp;-->
                 <!--                    <q-input v-model.number="options.molecular_weight.max" type="number" label="Max" filled style="max-width: 100px" />-->
                 <q-range v-model="options.molecular_weight" @change="onMWChange"
-                         :min="availableOptions.molecular_weight.min" style="width: 240px"
-                         :max="availableOptions.molecular_weight.max" label color="secondary"/>
+                         :min="staticOptions.molecular_weight.min" :max="staticOptions.molecular_weight.max"
+                         :inner-min="filteredAvailableOptions.molecular_weight.min" style="width: 240px"
+                         :inner-max="filteredAvailableOptions.molecular_weight.max" label color="secondary"/>
 
               </div>
               <div class="row q-px-xs q-py-xs filter-subsection-title">Isoelectric point:</div>
@@ -44,30 +52,54 @@
                 <!--                    <q-input v-model.number="options.isoelectric_point.min" type="number" label="Min" filled style="max-width: 100px" /> &nbsp; &nbsp;-->
                 <!--                    <q-input v-model.number="options.isoelectric_point.max" type="number" label="Max" filled style="max-width: 100px" />-->
                 <q-range v-model="options.isoelectric_point" @change="onpIChange"
-                         :min="availableOptions.isoelectric_point.min" style="width: 240px"
-                         :max="availableOptions.isoelectric_point.max" label color="secondary"/>
+                         :min="staticOptions.isoelectric_point.min" :max="staticOptions.isoelectric_point.max"
+                         :inner-min="filteredAvailableOptions.isoelectric_point.min" style="width: 240px"
+                         :inner-max="filteredAvailableOptions.isoelectric_point.max" label color="secondary"/>
               </div>
               <div class="row q-px-xs q-py-xs filter-subsection-title">Charge at pH 7.0:</div>
               <div class="row q-px-md">
                 <!--                    <q-input v-model.number="options.charge_at_pH_7.min" type="number" label="Min" filled style="max-width: 100px" /> &nbsp; &nbsp;-->
                 <!--                    <q-input v-model.number="options.charge_at_pH_7.max" type="number" label="Max" filled style="max-width: 100px" />-->
                 <q-range v-model="options.charge_at_pH_7" @change="onChargechange"
-                         :min="availableOptions.charge_at_pH_7.min" label style="width: 240px"
-                         :max="availableOptions.charge_at_pH_7.max" color="secondary"/>
+                         :min="staticOptions.charge_at_pH_7.min" :max="staticOptions.charge_at_pH_7.max"
+                         :inner-min="filteredAvailableOptions.charge_at_pH_7.min" label style="width: 240px"
+                         :inner-max="filteredAvailableOptions.charge_at_pH_7.max" color="secondary"/>
               </div>
-              <div class="row q-px-xs q-py-xs filter-subsection-title">Advanced filters</div>
-<!--              TODO　Add in database checking-->
-              <div class="row q-px-md q-py-xs">
-                <q-input v-model.number="options.family" type="text" label="Family" filled/>
-                <br/>
+              <div class="row q-px-xs q-py-xs filter-subsection-title">
+                 <q-toggle v-model="advancedFiltersVisible" label="Advanced filters" left-label class="q-mb-md" />
               </div>
-              <div class="row q-px-md q-py-xs">
-                <q-input v-model.number="options.sample" type="text" label="Sample/Genome" filled/>
-              </div>
+
+              <q-slide-transition>
+                <div v-show="advancedFiltersVisible">
+                  <!--  TODO　Add in database checking-->
+                  <div class="row q-px-md q-py-xs">
+                    <q-input v-model.number="options.family" type="text" label="Family" filled style="width: 250px"
+                             :error="(!familyInDB && options.family !== '')" lazy-rules
+                             @change="onFamilyChange" clearable @clear="onFamilyClear"
+                             error-message="The input does not follow any family" />
+                    <br/>
+                  </div>
+                  <div class="row q-px-md q-py-xs">
+                    <q-input v-model.number="options.sample" type="text" label="Sample/Genome" filled style="width: 250px"
+                             :error="(!sampleInDB && options.sample !== '')" lazy-rules
+                             @change="onSampleChange" clearable @clear="onSampleClear"
+                             error-message="The input does not follow any sample/genome" />
+                  </div>
+                </div>
+              </q-slide-transition>
             </div>
           </div>
 
           <div class="col-12 col-md-9 q-py-sm q-px-lg">
+            <div class="row">
+              <div class="col-6 main-text">Displaying: {{ info.pageSize }} out of {{ info.totalRow }} results.</div>
+              <div class="col-6" style="padding-right: 10rem;">
+                <el-button @click="downloadSearchResults" type="primary" class="download-btn">
+                  <BootstrapIcon icon="cloud-download" variant="light" size="1x" />
+                Download as CSV
+              </el-button>
+              </div>
+            </div>
             <!--            <q-page padding>-->
             <el-table :data="amps" stripe style="width: 100%" v-loading="loading">
               <el-table-column label="Accession" width="150%">
@@ -99,8 +131,8 @@
                 </template>
               </el-table-column>
             </el-table>
-            <el-pagination @size-change="setAMPsPageSize" @current-change="setAMPsPage" :page-sizes="[20, 50, 100, 200]"
-                           :page-size="20" layout="sizes, pager, jumper" :total="info.totalRow">
+            <el-pagination @size-change="setAMPsPageSize" @current-change="setAMPsPage" :page-sizes="[5, 20, 50, 100, 200]"
+                           :page-size="5" layout="sizes, pager, jumper" :total="info.totalRow">
             </el-pagination>
           </div>
         </div>
@@ -137,8 +169,9 @@ export default {
       charge_at_pH_7: {min: -57, max: 44}
     }
     return {
-      drawerOpen: true,
-      miniState: true,
+      advancedFiltersVisible: false,
+      familyInDB: true,
+      sampleInDB: true,
       loading: false,
       amps: [],
       axiosRefCount: 0,
@@ -150,8 +183,9 @@ export default {
         isoelectric_point: {min: 4, max: 12},
         charge_at_pH_7: {min: -57, max: 44}
       },
-      avalOptionsFull: options_full,
+      staticOptions: options_full,
       availableOptions: options_full,
+      filteredAvailableOptions: options_full
     }
   },
   setup() {
@@ -182,16 +216,14 @@ export default {
     });
   },
   mounted() {
-    this.setAMPsPageSize(20)
+    this.setAMPsPageSize(10)
     this.getAvailableOptions()
+    // this.grayOutOptions()
   },
   computed: {},
   methods: {
-    setAMPsPage(page) {
-      this.info.currentPage = page - 1
-      console.log(this.info.currentPage)
-      let config = {
-        params: {
+    getParams(){
+      return {
           family: this.options.family,
           habitat: this.options.habitat,
           host: this.options.host,
@@ -203,7 +235,13 @@ export default {
           charge_interval: this.options.charge_at_pH_7.min.toString() + ',' + this.options.charge_at_pH_7.max.toString(),
           page: this.info.currentPage,
           page_size: this.info.pageSize
-        }
+      }
+    },
+    setAMPsPage(page) {
+      this.info.currentPage = page - 1
+      console.log(this.info.currentPage)
+      let config = {
+        params: this.getParams()
       }
       let self = this
       this.axios.get('/amps', config)
@@ -222,91 +260,119 @@ export default {
       this.setAMPsPage(1)
     },
     getAvailableOptions() {
-      // this.showLoading()
       let self = this
       this.axios.get('/all_available_options')
           .then(function (response) {
             console.log(response.data)
-            self.avalOptionsFull = Object.assign({}, response.data)
+            self.staticOptions = Object.assign({}, response.data)
             self.availableOptions = response.data
           })
           .catch(function (error) {
             console.log(error);
           })
-      // this.closeLoading()
+    },
+    grayOutOptions(){
+      let config = {
+        params: this.getParams()
+      }
+      this.axios.get('/current_available_options', config)
+          .then(function (response) {
+            console.log(response.data)
+            self.filteredAvailableOptions = response.data
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+
     },
     // filterFamily(val, update, abort){
     //   update(() => {
     //     val = val.toLowerCase()
-    //     this.availableOptions.family = this.avalOptionsFull.family.filter(v => v.toLowerCase().indexOf(val) > -1)
+    //     this.availableOptions.family = this.staticOptions.family.filter(v => v.toLowerCase().indexOf(val) > -1)
     //   })
     // },
     // filterSample(val, update, abort){
     //   update(() => {
     //     val = val.toLowerCase()
-    //     this.availableOptions.sample = this.avalOptionsFull.sample.filter(v => v.toLowerCase().indexOf(val) > -1)
+    //     this.availableOptions.sample = this.staticOptions.sample.filter(v => v.toLowerCase().indexOf(val) > -1)
     //   })
     // },
     filterHabitat(val, update, abort) {
       update(() => {
         val = val.toLowerCase()
-        this.availableOptions.habitat = this.avalOptionsFull.habitat.filter(v => v.toLowerCase().indexOf(val) > -1)
+        this.availableOptions.habitat = this.staticOptions.habitat.filter(v => v.toLowerCase().indexOf(val) > -1)
       })
     },
     filterMicrobialSource(val, update, abort) {
       update(() => {
         val = val.toLowerCase()
-        this.availableOptions.microbial_source = this.avalOptionsFull.microbial_source.filter(v => v.toLowerCase().indexOf(val) > -1)
+        this.availableOptions.microbial_source = this.staticOptions.microbial_source.filter(v => v.toLowerCase().indexOf(val) > -1)
       })
     },
-    // onFamilyChange(option) {
-    //   this.options.family = option;
-    //   this.setAMPsPage(1)
-    // },
+    onFamilyChange(option) {
+      this.inDBChecking(this.options.family, 'family')
+      this.setAMPsPage(1)
+      // this.grayOutOptions()
+    },
+    onSampleChange(option) {
+      this.inDBChecking(this.options.sample, 'sample')
+      this.setAMPsPage(1)
+      // this.grayOutOptions()
+    },
+    onFamilyClear(){
+      this.options.family = ''
+      this.onFamilyChange(this.options.family)
+    },
+    onSampleClear(){
+      this.options.sample = ''
+      this.onSampleChange(this.options.sample)
+    },
     onHabitatChange(option) {
       this.options.habitat = option;
       this.setAMPsPage(1)
+      // this.grayOutOptions()
     },
-    // onSampleChange(option) {
-    //   this.options.sample = option;
-    //   this.setAMPsPage(1)
-    // },
     onMicrobialSourceChange(option) {
       this.options.microbial_source = option;
       this.setAMPsPage(1)
+      // this.grayOutOptions()
     },
     onPepLengthChange(value) {
       // this.options.pep_length = {min: 0, max: 100}
       console.log('peplength changed.')
       this.setAMPsPage(1)
+      // this.grayOutOptions()
     },
     onMWChange(value) {
       // this.options.pep_length = {min: 0, max: 100}
       console.log('MW changed.')
       this.setAMPsPage(1)
+      // this.grayOutOptions()
     },
     onpIChange(value) {
       // this.options.pep_length = {min: 0, max: 100}
       console.log('pI changed.')
       this.setAMPsPage(1)
+      // this.grayOutOptions()
     },
     onChargechange(value) {
       // this.options.pep_length = {min: 0, max: 100}
       console.log('Charge changed.')
       this.setAMPsPage(1)
+      // this.grayOutOptions()
     },
-    clearFilters() {
-      this.options = {
-        family: null,
-        habitat: null,
-        sample: null,
-        microbial_source: null,
-        pep_length: {
-          min: 0,
-          max: 100
-        }
-      }
-    },
+    // clearFilters() {
+    //   this.options = {
+    //     family: null,
+    //     habitat: null,
+    //     sample: null,
+    //     microbial_source: null,
+    //     pep_length: {
+    //       min: 0,
+    //       max: 100
+    //     }
+    //   }
+    // },
     initQualityTag(amps) {
       for (let i = 0; i < amps.length; i++) {
         Object.assign(amps[i], {quality_tag: {msg: 'Not available', level: 'warning'}})
@@ -359,6 +425,35 @@ export default {
         this.axiosRefCount--;
         this.loading = (this.axiosRefCount > 0);
       }
+    },
+    async downloadSearchResults() {
+      const ObjectsToCsv = require('objects-to-csv');
+      const data = new ObjectsToCsv(this.amps);
+      const str = await data.toString()
+      const blob = new Blob([str], {type: "text/plain;charset=utf-8"});
+      saveAs(blob, "AMPs.csv");
+    },
+    inDBChecking(val, entity_type){
+      self = this
+      this.axios.get('/in_db/' + entity_type + '/' + val)
+          .then(function (response) {
+            console.log(entity_type, val, response.data)
+            if (entity_type === 'family'){
+              self.familyInDB = response.data
+            }
+            if (entity_type === 'sample' || entity_type === 'genome'){
+              self.sampleInDB = response.data
+            }
+          })
+          .catch(function (error) {
+            console.log(error);
+            if (entity_type === 'family'){
+              self.familyInDB = false
+            }
+            if (entity_type === 'sample' || entity_type === 'genome'){
+              self.sampleInDB = false
+            }
+          })
     }
   }
 }
